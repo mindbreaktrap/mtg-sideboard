11314ddc9725a8a88ffbeefe0f509e0ed06570bb
mindbreaktrap <hello@henrikholm.de>
2023-10-31 13:59:44 +0100

    feat: improved boarding module

--- app/create_boardings.py
+++ app/create_boardings.py
@@ -1,30 +1,54 @@
 import json
 
 
-def boarding_in(opp: str, sideboard: dict) -> dict:
-    print("How many of each card are you bringing in?")
-    print(f"Opponent deck: {opp}")
-    result = {"in": {}}
-    for k, v in sideboard.items():
-        amount = int(input(f"{k} [0-{v}]: "))
+def boarding(opp: str, decklist: dict, boarding_type: str) -> dict:
+    if boarding_type == "in":
+        deck = decklist.get("sideboard")
+    elif boarding_type == "out":
+        deck = decklist.get("maindeck")
+    else:
+        raise ValueError("boarding_type must be either 'in' or 'out'")
+    print(f"How many of each card are you boarding {boarding_type.upper()} vs. {opp}?")
+    result = {}
+    for k, v in deck.items():  # type: ignore
+        try:
+            amount = int(input(f"{k} [0-{v}]: "))
+        except ValueError:
+            amount = 0
         if amount > 0:
-            result["in"][k] = amount
+            result[k] = amount
     return result
 
 
-def boarding_out(opp: str, maindeck: dict) -> dict:
-    return maindeck
+def full_boarding_for_matchup(opponent_deck: str, decklist: dict) -> dict:
+    result_dict = {
+        opponent_deck: {
+            "notes": input("Enter additional notes for the Matchup: "),
+            "play": {
+                "in": boarding(opponent_deck, decklist, "in"),  # type: ignore
+                "out": boarding(opponent_deck, decklist, "out"),  # type: ignore
+            },
+        }
+    }
+    if sum(result_dict[opponent_deck]["play"]["in"].values()) > sum(
+        result_dict[opponent_deck]["play"]["out"].values()
+    ):
+        print("You are boarding more cards in than out. Be careful!")
+        return result_dict
+    elif sum(result_dict[opponent_deck]["play"]["in"].values()) < sum(
+        result_dict[opponent_deck]["play"]["out"].values()
+    ):
+        raise ValueError("Boarding out more cards than in is not legal. Aborting!")
+    else:
+        print("Boarding is legal!")
+        return result_dict
 
 
 if __name__ == "__main__":
     print("This is a module, not a script.")
     print("Debugging...")
-    mb = {}
-    sb = {}
     with open("input/shadow.json", "r") as f:
         tmp_dict = json.loads(f.read())
-        mb = tmp_dict.get("decklist").get("maindeck")
-        sb = tmp_dict.get("decklist").get("sideboard")
-    opponent_deck = input("Enter opponent deck: ")
-    print(boarding_in(opponent_deck, sb))
-    print(boarding_out(opponent_deck, mb))
+        opponent_deck = input("Enter opponent deck (EXIT to finish): ")
+        while opponent_deck != "EXIT":
+            print(full_boarding_for_matchup(opponent_deck, tmp_dict.get("decklist")))
