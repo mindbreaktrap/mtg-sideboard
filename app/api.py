638105dd3e77e2a22ef2ae582f77f1b505982c06
mindbreaktrap <hello@henrikholm.de>
2023-11-05 19:14:48 +0100

    feat: documentation and root route

--- app/api.py
+++ app/api.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 from app.db import init
 from app.models.responses import BadRequest
@@ -13,7 +14,13 @@ async def lifespan(api: FastAPI):
     yield
 
 
-api = FastAPI(lifespan=lifespan)
+api = FastAPI(
+    title="Decklists and Boardings API",
+    description="This is a tool to generate decklists and boarding guides for Magic: The Gathering. It is currently in development.",
+    version="alpha",
+    lifespan=lifespan,
+)
+templates = Jinja2Templates(directory="app/templates")
 
 
 # exception handler for pydantic value errors
@@ -24,3 +31,16 @@ async def validation_exception_handler(request: Request, exc: ValueError):
 
 api.include_router(boardings.router)
 api.include_router(decklists.router)
+
+
+# Serve a static HTML Frontend
+@api.get("/")
+async def root(request: Request):
+    content = {
+        "request": request,
+        "title": "Boarding Guide Generator",
+        "heading": "Decklist and Boarding Guide Generator",
+        "paragraph": "This is a tool to generate decklists and boarding guides for Magic: The Gathering. It is currently in development.",
+        "swagger_url": "/docs",
+    }
+    return templates.TemplateResponse("index.html", content)
