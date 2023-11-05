e61270f71604e0de52530f20170278869b7a680e
mindbreaktrap <hello@henrikholm.de>
2023-11-05 19:00:29 +0100

    feat: tests for boarding validation

--- app/models/boardings.py
+++ app/models/boardings.py
@@ -7,7 +7,11 @@ from pydantic import BaseModel, Field, model_validator
 
 class BoardingInput(BaseModel):
     deckname: str = Field(..., examples=["Death's Shadow"])
-    deck_id: Optional[uuid.UUID] = Field(default=None, examples=[str(uuid.uuid4())])
+    deck_id: Optional[uuid.UUID] = Field(
+        default=None,
+        examples=[str(uuid.uuid4())],
+        description="Optional ID of the deck the boarding belongs to.",
+    )
     """The endpoint will validate that this exists."""
     opponent: str = Field(..., examples=["Jeskai Control"])
     on_the_play: bool = Field(True, examples=[True, False])
@@ -34,9 +38,15 @@ class BoardingInput(BaseModel):
     # validate that we are not boarding out more than we are boarding in
     @model_validator(mode="after")
     def validate_boarding(self) -> "BoardingInput":
-        cards_coming_in = self.cards_coming_in
-        cards_going_out = self.cards_going_out
-        if sum(cards_coming_in.values()) < sum(cards_going_out.values()):
+        cards_coming_in = sum(self.cards_coming_in.values())
+        if cards_coming_in == 0:
+            raise ValueError("You must board in at least one card.")
+        if cards_coming_in > 15:
+            raise ValueError("You cannot board in more than 15 cards.")
+        cards_going_out = sum(self.cards_going_out.values())
+        if cards_going_out > 15:
+            raise ValueError("You cannot board out more than 15 cards.")
+        if cards_coming_in < cards_going_out:
             raise ValueError(
                 "You are boarding out more cards than you are boarding in."
             )
@@ -48,3 +58,109 @@ class Boarding(BoardingInput, Document):
 
     class Settings:
         name = "boardings"
+
+
+if __name__ == "__main__":
+    # Test with too many cards going out
+    try:
+        boarding = BoardingInput(
+            deckname="Death's Shadow",
+            opponent="Jeskai Control",
+            on_the_play=True,
+            cards_coming_in={"Brazen Borrower": 1, "Dauthi Voidwalker": 1},
+            cards_going_out={"Thoughtseize": 2, "Force of Will": 1},
+        )
+    except ValueError:
+        print("Boarding validation failed as expected.")
+    else:
+        raise AssertionError("Boarding validation should have failed.")
+
+    # Test with valid boarding
+    try:
+        boarding = BoardingInput(
+            deckname="Death's Shadow",
+            opponent="Jeskai Control",
+            on_the_play=True,
+            cards_coming_in={
+                "Brazen Borrower": 1,
+                "Dauthi Voidwalker": 1,
+                "Dress Down": 1,
+            },
+            cards_going_out={"Thoughtseize": 2, "Force of Will": 1},
+        )
+        print("Boarding validation passed as expected.")
+    except ValueError:
+        raise AssertionError("Boarding validation should have passed.")
+
+    # Test with 16 cards going in
+    try:
+        boarding = BoardingInput(
+            deckname="Death's Shadow",
+            opponent="Jeskai Control",
+            on_the_play=True,
+            cards_coming_in={
+                "Brazen Borrower": 4,
+                "Dauthi Voidwalker": 4,
+                "Dress Down": 4,
+                "Fatal Push": 4,
+            },
+            cards_going_out={"Thoughtseize": 2, "Force of Will": 1},
+        )
+    except ValueError:
+        print("Boarding validation failed as expected.")
+    else:
+        raise AssertionError("Boarding validation should have failed.")
+
+    # Test with 16 cards going out
+    try:
+        boarding = BoardingInput(
+            deckname="Death's Shadow",
+            opponent="Jeskai Control",
+            on_the_play=True,
+            cards_coming_in={
+                "Brazen Borrower": 1,
+                "Dauthi Voidwalker": 1,
+                "Dress Down": 1,
+            },
+            cards_going_out={
+                "Thoughtseize": 4,
+                "Force of Will": 4,
+                "Fatal Push": 4,
+                "Drown in the Loch": 4,
+            },
+        )
+    except ValueError:
+        print("Boarding validation failed as expected.")
+    else:
+        raise AssertionError("Boarding validation should have failed.")
+
+    # Test with empty boarding
+    try:
+        boarding = BoardingInput(
+            deckname="Death's Shadow",
+            opponent="Jeskai Control",
+            on_the_play=True,
+            cards_coming_in={},
+            cards_going_out={},
+        )
+    except ValueError:
+        print("Boarding validation failed as expected.")
+    else:
+        raise AssertionError("Boarding validation should have failed.")
+
+    # Test with cards going in but no cards coming out
+    try:
+        boarding = BoardingInput(
+            deckname="Death's Shadow",
+            opponent="Jeskai Control",
+            on_the_play=True,
+            cards_coming_in={
+                "Brazen Borrower": 1,
+                "Dauthi Voidwalker": 1,
+                "Dress Down": 1,
+            },
+            cards_going_out={},
+        )
+        print("Boarding validation passed as expected.")
+    except ValueError:
+        raise AssertionError("Boarding validation should have passed.")
